85482ebfcb21479f7cd94ecf90ecc7c9a614d89c
Samppa Salo <samppasalo16@gmail.com>
2026-06-09 17:51:09 +0300

    fix database restart loop with older version and add user saving

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,9 +1,21 @@
 const bcrypt = require("bcrypt")
 const usersRouter = require("express").Router()
+const User = require("../models/user")
 
 usersRouter.post("/", async (req, res) => {
     const { username, password } = req.body
-    console.log(username, password)
+    
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    res.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
